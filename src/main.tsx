e1438f936fcc03bf638f208e921f105eb287e475
user.email <cam@giveabit.io>
2026-09-25 23:58:45 +0200

    kimi: wire Sentry error reporting (wired-but-off, VITE_SENTRY_DSN)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,26 @@ import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
 import './index.css'
 
+declare global {
+  interface Window {
+    __MOTOPASS_SENTRY__?: any
+  }
+}
+
+if (import.meta.env.VITE_SENTRY_DSN) {
+  import('@sentry/react').then((Sentry) => {
+    window.__MOTOPASS_SENTRY__ = Sentry
+    Sentry.init({
+      dsn: import.meta.env.VITE_SENTRY_DSN,
+      integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+      tracesSampleRate: 0.3,
+      replaysSessionSampleRate: 0.1,
+      replaysOnErrorSampleRate: 1.0,
+      environment: import.meta.env.MODE
+    })
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
